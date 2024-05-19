b5aa697041ab118afa3d9e2bbef6b1dabe5909b1
Siam Rahman <asifur.web@gmail.com>
2024-05-20 01:24:30 +0600

    register work in progress signing off

--- src/components/Pages/Regsiter/Register.jsx
+++ src/components/Pages/Regsiter/Register.jsx
@@ -1,12 +1,25 @@
 const Register = () => {
   return (
     <div>
-      <form className="">
-        <div className="bg-slate-400 ">
-          <input type="email" />
-        </div>
-        <div className="bg-slate-400 ">
-          <input type="password" />
+      <form className="max-w-screen-sm mx-auto ">
+        <div className=" flex flex-col justify-center items-center p-16 bg-slate-200 ">
+          <div className="mb-8">
+            <p className="text-slate-400 mx-2 text-sm">EMAIL ADDRESS</p>
+            <input
+              className="py-1 px-5 rounded-2xl"
+              type="email"
+              name="email"
+            />
+          </div>
+
+          <div className=" ">
+            <p className="text-slate-400 mx-2 text-sm">PASSWORD</p>
+            <input
+              className="py-1 px-5 rounded-2xl"
+              type="password"
+              name="password"
+            />
+          </div>
         </div>
       </form>
     </div>
